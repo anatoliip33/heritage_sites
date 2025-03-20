f8d312f82dcd14b0a648a1cbbcc90c15d2beca86
anatolii <anatoliipolishchuk@gmail.com>
2025-03-20 14:20:53 +0200

    first commit with gitignore

---
+++ lib/heritage_sites.ex
@@ -0,0 +1,18 @@
+defmodule HeritageSites do
+  @moduledoc """
+  Documentation for `HeritageSites`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HeritageSites.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/heritage_sites_test.exs
@@ -0,0 +1,8 @@
+defmodule HeritageSitesTest do
+  use ExUnit.Case
+  doctest HeritageSites
+
+  test "greets the world" do
+    assert HeritageSites.hello() == :world
+  end
+end
